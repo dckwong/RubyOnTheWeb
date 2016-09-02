1a5f372f3c6c48e7cfd9c6b8bea2d3e9edefc423
Derek <derek_kwong8@yahoo.com>
2016-09-02 11:53:14 -0700

    Started POST requests

--- simple_server_practice/simple_server.rb
+++ simple_server_practice/simple_server.rb
@@ -1,23 +1,48 @@
 require 'socket'
 
-server = TCPServer.open(2345)
+def do_GET(client,response)
+	path = /\S+/.match(response[4..-1])
+	if File.exist?(".#{path.to_s}")
+		File.open(".#{path.to_s}",'r') do |file|
+			client.print("HTTP/1.1 200 OK\r\n" + "Date: #{Time.now.ctime}\r\n" + "Content-Type: text/html\r\n" + "Content-Length: #{response.length}\r\n\r\n")
+			client.print(file.readlines.join(""))
+		end
+	else
+		client.print "HTTP/1.1 404 Not Found\r\n\r\n"
+		client.print "404 File Not Found"
+	end
+end
 
+def check_GET_or_POST(client,response)
+	check_GET = /GET\s+.+\s+HTTP\/\d\.\d/.match(response)
+	check_POST = /POST\s+.+\s+HTTP\/\d\.\d/.match(response)
+	if check_GET
+		return "GET"
+	elsif check_POST
+		return "POST"
+	else
+		return "Neither"
+	end
+end
+
+def do_POST(client,response)
+	
+end
+
+server = TCPServer.open(2345)
 loop do
 	client = server.accept
-	response = client.gets
-	check = /GET\s+.+\s+HTTP\/\d\.\d/.match(response)
-	if check
-		path = /\S+/.match(response[4..-1])
-		match = /HTTP.+/.match(response)
-		if path.to_s == "/index.html"
-			client.print "#{match} 200 OK \r\n\r\n"
-			client.print "Date: #{Time.now.ctime}\n"
-			client.print "Content-Type: text/html\n"
-			client.print "Content-Length: #{response.length}\n"
-		else
-			client.print "#{match} 404 Not Found"
-		end
-	end
-	client.puts("Closing connection now.")
+	response = client.get
+	case check_GET_or_POST(client,response)
+	when "GET"
+		do_GET(client,response)
+	when "POST"
+		do_POST(client,response)
+	when "Neither"
+		client.puts("Not a valid request")
 	client.close
 end
+
+
+
+
--- simple_server_practice/tiny_web_browser.rb
+++ simple_server_practice/tiny_web_browser.rb
@@ -1,16 +1,62 @@
 require 'socket'
+require 'json'
+
+def select_request
+	begin
+		puts "Do you want to make a 'GET' request or 'POST' request?"
+		choice = gets.chomp.upcase
+		raise  StandardError if choice != "GET" && choice != "POST"
+	rescue StandardError
+		puts "Type 'POST' or 'GET'"
+		retry
+	else
+		choice
+	end
+end
+
+def get_info
+	puts "What's your viking's name?"
+	name = gets.chomp
+	puts "What's your viking's email?"
+	email = gets.chomp
+	viking_hash[:viking] = {
+		:name => name,
+		:email => email
+	}
+	viking_hash
+end
+
+def make_GET(socket)
+	path = "/index.html"
+	request = "GET #{path} HTTP/1.0\r\n\r\n"
+	socket.print(request)
+	socket.read
+end
+
+def make_POST(socket)
+	path = "/thanks.html"
+	get_info
+	content = viking_hash.to_json
+	request = "POST #{path} HTTP/1.0\n" + "From: #{viking_hash[:viking][:email]}\n" + "Content-Type: application/x-www-form-urlencoded\n" + "Content-Length: #{content.length}\r\n\r\n"
+
+
+end
 
 host = "localhost"
 port = 2345
-path = "/index.html"
-
-
-request = "GET #{path} HTTP/1.0\r\n\r\n"
-
 socket = TCPSocket.open(host,port)
-socket.print(request)
-response = socket.read
-headers, body = response.split("\r\n\r\n", 2)
+choice = select_request
+viking_hash = {}
+viking_hash[:viking] = {}
+case choice
+when "GET"
+	response = make_GET(socket)
+	headers, body = response.split("\r\n\r\n", 2)
+when "POST"
+
+end
+
+
 #print headers
 #print body
-print response
+#print response
